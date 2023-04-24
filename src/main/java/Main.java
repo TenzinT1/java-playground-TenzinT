3a56609fe7b65d7e410a13e1ae8b42affa014019
TenzinT1 <tentash2222@gmail.com>
2023-04-23 23:10:05 -0700

    Finished 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Cat coco = new Cat("coco", 14);
-        Dog max = new Dog("max", 5, "Peng");
+        Dog max = new Dog("max", 5, "Tenzin");
         Rabbit rabbit = new Rabbit("snowball", 3, "black");
         Toy robot = new Toy("curious George");
 
